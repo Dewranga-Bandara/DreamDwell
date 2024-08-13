fb624256c8e98a0e4c19e429338ef927eee9c8ee
Dewranga-Bandara <dewrangabandara071@gmail.com>
2024-08-13 18:07:47 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+  <h1 className='text-3xl font-bold mb-4 text-slate-800'>About DreamDwell</h1>
+  <p className='mb-4 text-slate-700'>
+    DreamDwell is your go-to platform for finding your perfect home. We make the process of buying, selling, and renting properties easy and enjoyable. Our expert team is here to guide you every step of the way, ensuring a smooth and satisfying experience.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Our goal is to help you achieve your real estate dreams with personalized service and in-depth local knowledge. Whether you are searching for a new home, selling your property, or looking to rent, DreamDwell is here to make it happen.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    With a dedicated team of professionals, we turn the real estate journey into an exciting adventure. At DreamDwell, your dream home is just a step away.
+  </p>
+</div>
+
   )
 }
