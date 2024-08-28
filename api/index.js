60b90c090a55fa4953862a34b17f209ca9fbd31c
Dewranga-Bandara <dewrangabandara071@gmail.com>
2024-08-28 22:21:45 +0530

    Update index.js and vercel.json

--- api/index.js
+++ api/index.js
@@ -1,31 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
-dotenv.config()
+dotenv.config();
 
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
-        console.log("Connected to MongoDB!")
-    }).catch((err) => {
-        console.log(err)
+        console.log("Connected to MongoDB!");
     })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err.message);
+    });
 
 const app = express();
 
-app.use(express.json())
-
-app.use(cookieParser())
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-    }
-)
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
@@ -35,8 +30,17 @@ app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
+        success: false,
+        statusCode,
+        message,
     });
-  });
+});
+
+// Export the Express app as a module to be used by Vercel's serverless functions
+export default app;
+
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000!');
+//     }
+// )
