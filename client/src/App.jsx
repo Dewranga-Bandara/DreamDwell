2711ae6d151b8f0dec7e75810e72091ffdeb1816
Dewranga-Bandara <dewrangabandara071@gmail.com>
2024-08-28 22:43:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
@@ -15,24 +16,28 @@ import Offers from './pages/Offers';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home />} />        
-        <Route path='/sign-in' element={<SignIn />} />        
-        <Route path='/sign-up' element={<SignUp />} />        
-        <Route path='/about' element={<About />} /> 
-        <Route path='/offers' element={<Offers />} /> 
-        <Route path='/search' element={<Search />} />
-        <Route path='/listing/:listingId' element={<Listing />} />       
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />  
-          <Route path='/update-listing/:listingId' element={<UpdateListing />}
-          />   
-        </Route>  
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home />} />        
+          <Route path='/sign-in' element={<SignIn />} />        
+          <Route path='/sign-up' element={<SignUp />} />        
+          <Route path='/about' element={<About />} /> 
+          <Route path='/offers' element={<Offers />} /> 
+          <Route path='/search' element={<Search />} />
+          <Route path='/listing/:listingId' element={<Listing />} />       
+          <Route element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/create-listing' element={<CreateListing />} />  
+            <Route path='/update-listing/:listingId' element={<UpdateListing />}
+            />   
+          </Route>  
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+      <Analytics />
+    </>
+    
   );
 }
